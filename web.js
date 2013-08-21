80c916ad6cb176da2c41bade37d6c0b6400f413c
Ben Kobler <benkobler@alumni.cmu.edu>
2013-08-20 20:06:41 -0400

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
+var fs = require('fs');
+var buffer1
+var buffer2
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
+buffer = fs.readFileSync('index.html');
+buffer2 = buffer.toString('utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer2);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
